8d5c6b3ca55b3d59ba5b21d006fd59d568acddb2
Martin Vuille <jpmv27@yahoo.com>
2016-08-27 11:12:49 -0400

    ClipboardObserver.java: Detect and broadcast 'empty' clipboard

--- src/net/dumbinter/netclip/ClipboardObserver.java
+++ src/net/dumbinter/netclip/ClipboardObserver.java
@@ -1,30 +1,42 @@
 package net.dumbinter.netclip;
 
 import java.awt.datatransfer.DataFlavor;
+import java.awt.datatransfer.Transferable;
 import java.awt.datatransfer.UnsupportedFlavorException;
 import java.io.IOException;
 import java.lang.IllegalStateException;
 
 public class ClipboardObserver implements Runnable {
-	
-	
+
+
 	UDPServer broadcaster = null;
-	
+
 	public ClipboardObserver(UDPServer broadcaster) {
 		this.broadcaster=broadcaster;
 	}
-	
+
 	public void run() {
 		System.out.println("Observer running");
 		while(true){
 			try {
-				if(!NetClipboard.isListenOnly() && NetClipboard.getClipboard().isDataFlavorAvailable(DataFlavor.stringFlavor)){
-					final String clipboardData = (String) NetClipboard.getClipboard().getData(DataFlavor.stringFlavor);
-					if (NetClipboard.getLastData().hashCode() != clipboardData.hashCode()) {
-						NetClipboard.setLastData(clipboardData);
-						NetClipboard.setData(clipboardData.getBytes("UTF-8"));
-						System.out.println("Local change detected: "+ clipboardData);
-						broadcaster.broadcast();
+				if (!NetClipboard.isListenOnly()) {
+					final Transferable clipboardRaw = NetClipboard.getClipboard().getContents(null);
+					String clipboardData = null;
+
+					if ((clipboardRaw == null) || (clipboardRaw.getTransferDataFlavors().length == 0)) {
+						clipboardData = "";
+					}
+					else if (clipboardRaw.isDataFlavorSupported(DataFlavor.stringFlavor)) {
+						clipboardData = (String) clipboardRaw.getTransferData(DataFlavor.stringFlavor);
+					}
+
+					if (clipboardData != null) {
+						if (NetClipboard.getLastData().hashCode() != clipboardData.hashCode()) {
+							NetClipboard.setLastData(clipboardData);
+							NetClipboard.setData(clipboardData.getBytes("UTF-8"));
+							System.out.println("Local change detected: \"" + clipboardData + "\"");
+							broadcaster.broadcast();
+						}
 					}
 				}
 			} catch (UnsupportedFlavorException | IOException e) {
@@ -33,7 +45,7 @@ public class ClipboardObserver implements Runnable {
 			} catch (IllegalStateException e) {
 				// The clipboard is busy at the moment
 				// Ignore it, we'll try again next time
-			} 
+			}
 			try {
 				Thread.sleep(100);
 			} catch (InterruptedException e) {
